d4f088c14ea901668440e28264c73f6844206f19
nailseong <nnis9857@gmail.com>
2020-09-22 21:53:29 +0900

    AuthHome

--- screens/Auth/AuthHome.js
+++ screens/Auth/AuthHome.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { screenWidth, screenHeight } from "../../constants";
 
 const View = styled.View`
   flex: 1;
@@ -8,16 +8,46 @@ const View = styled.View`
   align-items: center;
 `;
 
-const Text = styled.Text``;
+const Image = styled.Image`
+  width: ${screenWidth};
+  height: ${screenHeight / 18};
+`;
+
+const Touchable = styled.TouchableOpacity``;
+const SignUpBtn = styled.View`
+  background-color: ${(props) => props.theme.blueColor};
+  padding: 6px;
+  margin: 24px 0;
+  width: ${screenWidth / 2};
+  border-radius: 3px;
+`;
+const SignUpBtnText = styled.Text`
+  color: white;
+  text-align: center;
+  font-weight: 600;
+`;
+
+const LoginLink = styled.View``;
+const LoginLinkText = styled.Text`
+  color: ${(props) => props.theme.blueColor};
+  font-weight: 600;
+`;
 
 export default ({ navigation }) => (
   <View>
-    <Text>AuthHome</Text>
-    <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-      <Text>Go to Log In</Text>
-    </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
-      <Text>Go to Sign Up</Text>
-    </TouchableOpacity>
+    <Image
+      resizeMode={"contain"}
+      source={require("../../assets/instagramLogo.png")}
+    />
+    <Touchable onPress={() => navigation.navigate("Signup")}>
+      <SignUpBtn>
+        <SignUpBtnText>Create New Account</SignUpBtnText>
+      </SignUpBtn>
+    </Touchable>
+    <Touchable onPress={() => navigation.navigate("Login")}>
+      <LoginLink>
+        <LoginLinkText>Log In</LoginLinkText>
+      </LoginLink>
+    </Touchable>
   </View>
 );
