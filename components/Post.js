6e833bc71ab7db59fbadf50a2f5ced8ae4c4f63c
nailseong <nnis9857@gmail.com>
2020-09-25 00:24:43 +0900

    Post Component

--- components/Post.js
+++ components/Post.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Image } from "react-native";
+import Swiper from "react-native-swiper";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import { Ionicons } from "@expo/vector-icons";
+
+import { screenWidth } from "../constants";
 
 const Container = styled.View``;
 const Header = styled.View`
   flex-direction: row;
   align-items: center;
+  justify-content: flex-start;
   padding: 8px;
 `;
 
@@ -14,7 +19,6 @@ const Touchable = styled.TouchableOpacity``;
 const Bold = styled.Text`
   font-weight: 500;
   font-size: 14px;
-  margin-left: 2px;
 `;
 
 const Location = styled.Text`
@@ -22,7 +26,37 @@ const Location = styled.Text`
   font-size: 12px;
 `;
 
-const Post = ({ user, location }) => {
+const IconsContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 6px;
+`;
+
+const IconContainer = styled.View`
+  margin-right: 12px;
+`;
+
+const InfoContainer = styled.View`
+  padding: 12px;
+`;
+
+const Caption = styled.Text`
+  margin: 4px 0;
+`;
+
+const CommentsCount = styled.Text`
+  opacity: 0.5;
+  font-size: 12px;
+`;
+
+const Post = ({
+  user,
+  location,
+  files = [],
+  likeCount,
+  caption,
+  comments = [],
+}) => {
   return (
     <Container>
       <Header>
@@ -37,6 +71,51 @@ const Post = ({ user, location }) => {
           {location ? <Location>{location}</Location> : null}
         </Touchable>
       </Header>
+      <Swiper height={screenWidth} loop={false}>
+        {files.map((photo) => (
+          <Image
+            key={photo.id}
+            style={{
+              width: screenWidth,
+              height: screenWidth,
+            }}
+            source={{ uri: photo.url }}
+          />
+        ))}
+      </Swiper>
+      <InfoContainer>
+        <IconsContainer>
+          <Touchable>
+            <IconContainer>
+              <Ionicons
+                name={
+                  Platform.OS === "ios" ? "ios-heart-empty" : "md-heart-empty"
+                }
+                size={30}
+              />
+            </IconContainer>
+          </Touchable>
+          <Touchable>
+            <IconContainer>
+              <Ionicons
+                name={Platform.OS === "ios" ? "ios-text" : "md-text"}
+                size={30}
+              />
+            </IconContainer>
+          </Touchable>
+        </IconsContainer>
+        <Touchable>
+          <Bold>
+            {likeCount === 1 ? ` ${likeCount} like` : `${likeCount} likes`}
+          </Bold>
+        </Touchable>
+        <Caption>
+          <Bold>{user.username}</Bold> {caption}
+        </Caption>
+        <Touchable>
+          <CommentsCount>See all {comments.length} comments</CommentsCount>
+        </Touchable>
+      </InfoContainer>
     </Container>
   );
 };
